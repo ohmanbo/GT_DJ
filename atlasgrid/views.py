c92aead4888b31db41587aaccd6e29fa395871de
sungwoo <ohmanbo@gmail.com>
2024-02-09 15:07:11 +1100

    chat basic

--- atlasgrid/views.py
+++ atlasgrid/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-#from django.http.response import HttpResponse
+from django.http.response import HttpResponseRedirect
 from .models import account, grid, HelloWorld
 from rest_framework import viewsets
+from django.urls import reverse
 
 
 class AtlasGridInfo( viewsets.ModelViewSet ):
@@ -11,12 +12,9 @@ class AtlasGridInfo( viewsets.ModelViewSet ):
 
 # Create your views here.
 def view(request):
-    #return HttpResponse("처리할 데이터가 없습니다.")
     userlist = account.objects.all()
     context = {'userlist': userlist}
     
-    #return render(request, 'view.html', context)
-    #return render(request, 'base.html', context)
     if request.method == 'POST':
         
         temp = request.POST.get('monitor_input')
@@ -25,7 +23,9 @@ def view(request):
         new_hello_world.text = temp
         new_hello_world.save()
         
-        return render(request, 'atlasgrid/monitor.html', context={'monitor_output': new_hello_world})
+        return HttpResponseRedirect(reverse('monitor'))
     
     else:
-        return render(request, 'atlasgrid/monitor.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        hello_world_list_reversed = hello_world_list[::-1]
+        return render(request, 'atlasgrid/monitor.html', context={'monitor_output': hello_world_list_reversed})
